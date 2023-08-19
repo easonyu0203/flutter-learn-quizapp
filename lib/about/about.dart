2b520c4d8df005fa902252315f385c05afdf28b3
easonyu0203 <breaking0203@gmail.com>
2023-08-19 17:55:42 +1000

    first commit

---
+++ lib/about/about.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class AboutHome extends StatelessWidget {
+  const AboutHome({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+    );
+  }
+}
